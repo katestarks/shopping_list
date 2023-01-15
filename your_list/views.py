7a6fba316add777f46dee3d3ec67144b29a16fb0
katestarks <katestarks@me.com>
2023-01-15 17:20:55 +0000

    updated views to add basket total

--- your_list/views.py
+++ your_list/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from .src.CostsController import CostsController
 
 
 # Home page view which displays shopping list items
 def index(request):
     list = {
-        "item_1": "Lotus",
-        "item_2": "Aeropex",
-        "item_3": "Garmin",
-        "item_4": "Ski Lodge",
-        "item_5": "New Puppy"}
-    return render(request, 'your_list/index.html', {"list": list})
+        "item_1": {"item": "Lotus", "cost": 60_000},
+        "item_2": {"item": "Aeropex", "cost": 165},
+        "item_3": {"item": "Garmin", "cost": 585},  
+        "item_4": {"item": "Ski Lodge", "cost": 1_200_000},
+        "item_5": {"item": "New Puppy", "cost": 120}}
+    
+    costs_controller = CostsController()
+    costs_controller.calculate_basket_total(list)
+    costs_controller.int_to_number_format_string(costs_controller.basket_total)
+
+    context = {"list": list, "total": costs_controller.number_formatted_string}
+
+    return render(request, 'your_list/index.html', context)
