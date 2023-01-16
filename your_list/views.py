50861dfd1284494a43a67e210afff0578109b800
katestarks <katestarks@me.com>
2023-01-16 08:22:29 +0000

    Updated views to use helpers

--- your_list/views.py
+++ your_list/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
 from .src.CostsController import CostsController
+from .src.ItemsController import ItemsController
+from .models import List_Items
 
 
 # Home page view which displays shopping list items
 def index(request):
-    list = {
-        "item_1": {"item": "Lotus", "cost": 60_000},
-        "item_2": {"item": "Aeropex", "cost": 165},
-        "item_3": {"item": "Garmin", "cost": 585},  
-        "item_4": {"item": "Ski Lodge", "cost": 1_200_000},
-        "item_5": {"item": "New Puppy", "cost": 120}}
+    items_to_display = List_Items.get_active_items()
+
+    items_controller = ItemsController()
+    items_controller.adds_items_to_dictionary(items_to_display)
     
     costs_controller = CostsController()
-    costs_controller.calculate_basket_total(list)
+    costs_controller.get_item_costs(items_to_display)
+    costs_controller.calculate_basket_total(costs_controller.item_costs)
     costs_controller.int_to_number_format_string(costs_controller.basket_total)
 
-    context = {"list": list, "total": costs_controller.number_formatted_string}
+    context = {
+        "list": items_controller.item_names_dictionary,
+        "total": costs_controller.number_formatted_string}
 
     return render(request, 'your_list/index.html', context)
